63c31db72a60cddb2ce079e63f7ea97ce7bbe079
Pallav Priyadarshi <palav729@gmail.com>
2015-09-22 22:47:07 -0700

    reverse integer

---
+++ leetcode/reverseinteger.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int Reverse(int x) {
+        bool isNegative = false;
+        if(x < 0){
+            x = x*-1;
+            isNegative = true;
+        }
+        
+        long y = 0;
+        while(x > 0){
+            int digit = x%10;
+            y = y*10 + digit;
+            x = x/10;
+        }
+        
+        if(y>Int32.MaxValue){
+            return 0;
+        }
+        
+        if(isNegative){
+            y = y*-1;
+        }
+        
+        return Convert.ToInt32(y);
+    }
+}
